4b5c66050265c9729165626fc23cf14cad849ff9
lml <634131831@qq.com>
2019-10-25 12:18:03 +0800

    [build]reportng改成调用本地的版本

--- src/test/resources/script/biz/bizHelper.js
+++ src/test/resources/script/biz/bizHelper.js
@@ -2,9 +2,21 @@ $(function () {
     (function (obj) {
         class BizObj {
 
+            static getTheTableData(tableDom) {
+                let result = [];
+                let table = domHelper.domObj.getDomByScript(tableDom);
+                let trs = table.find('tr');
+                trs.each((idx, item) => {
+                    let data = $(item).find('td').eq(1).text().trim();
+                    result.push(data);
+                });
+                return result;
+            }
+
+
         }
 
         obj.bizHelper = obj.bizHelper || {};
-        obj.bizHelper.uploadObj = BizObj;
+        obj.bizHelper.bizObj = BizObj;
     })(window);
 });
