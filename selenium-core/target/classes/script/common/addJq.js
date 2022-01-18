d0eabdb99549e3eee184161cdc0bf54237abe55a
lml <634131831@qq.com>
2022-01-18 21:49:41 +0800

    [build]去除target文件夹

--- selenium-core/target/classes/script/common/addJq.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    if (!window.jQuery) {
-        let s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = '%s';
-        document.body.appendChild(s);
-    }
-})();
--- selenium-core/target/classes/script/common/getChildFrames.js
+++
@@ -1,8 +0,0 @@
-function getChildFrames() {
-    let arr = [];
-    let iFrames = document.getElementsByTagName("iframe");
-    for (let i = 0; i < iFrames.length; i++) {
-        arr.push(iFrames[i].src);
-    }
-    return arr;
-}
